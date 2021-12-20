c028943b8b9df5046b88f1b8e2bd5f5957155368
Fatemeh Brizendine <ssaberi324@yahoo.com>
2021-12-19 17:32:02 -0800

    add server.js

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+const { getNotes, saveNote, deleteNote, renderActiveNote, handleNoteSave, handleNoteDelete, handleNoteView,
+handleNewNoteView, handleRenderSaveBtn, renderNoteList, getAndRenderNotes } = require('../../public/assets/js/index');
 
 router.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+        results = getNotes(req.query, results);
     }
     res.json(results);
 });
 
-router.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+// router.post('/api/notes', (req, res) => {
+//     req.body.id = notes.length.toString();
 
-    if (!validate)
-})
+//     if ()
+// })
 
 module.exports = router;
